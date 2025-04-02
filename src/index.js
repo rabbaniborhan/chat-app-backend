5e3843a1aaeb424664210452c2f29c5a344b40fb
Borhan <brborhan70@gmail.com>
2025-04-03 02:13:39 +0600

    vercel file update because cors error

--- src/index.js
+++ src/index.js
@@ -30,6 +30,13 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Change '*' to allowed domains if needed
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
