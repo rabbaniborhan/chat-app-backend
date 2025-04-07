d8eff8bccf83fbed7657959942dc4c24878ea8fe
Borhan <brborhan70@gmail.com>
2025-04-07 15:21:29 +0600

    check console for render

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,6 +4,7 @@ import User from "../models/use.model.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req?.cookies?.jwt;
+    console.log(token);
     if (!token) {
       return res
         .status(401)
